1be267bc26d920cd0c1e58b31121114cf8f958bf
Matthew Metzger <mmetzger@my.bellevue.edu>
2019-07-01 11:45:15 -0400

    added GameBoard component base.

---
+++ src/components/TicTacToe/GameBoard/GameBoard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './GameBoard.css';
+
+export default class GameBoard extends Component {
+    render() {
+        return(
+            <div className={"gameboard"}>
+                GameBoard Component
+            </div>
+        )
+    }
+}

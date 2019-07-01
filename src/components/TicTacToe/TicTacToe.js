4c79d5968419cfc8720f4a9c4de415d6f778df5f
Matthew Metzger <mmetzger@my.bellevue.edu>
2019-07-01 11:29:28 -0400

    added primary game component container (TicTacToe)

---
+++ src/components/TicTacToe/TicTacToe.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './TicTacToe.css';
+
+export default class TicTacToe extends Component {
+    render() {
+        return(
+            <div className={"tictactoe"}>
+                TicTacToe Component
+            </div>
+        )
+    }
+}

f27356203c4f5fc5767646e1fb8660c1d3198054
Matthew Metzger <mmetzger@my.bellevue.edu>
2019-07-01 11:12:13 -0400

    test - confirm existence of game component (TicTacToe).

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {mount} from 'enzyme'
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+const app = mount(<App />);
+it('loads TicTacToe component', () => {
+  expect(app.find('TicTacToe')).toHaveLength(1);
 });
 

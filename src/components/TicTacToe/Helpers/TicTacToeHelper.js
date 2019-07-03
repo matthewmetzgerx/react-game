d90bc7ccf7a62268eabc71962dd84f59b9931510
Matthew Metzger <mmetzger@my.bellevue.edu>
2019-07-03 19:52:08 -0400

    Fixed helper tests and added helper methods for getting/setting state object and determining which player's turn based on the number of moves.

--- src/components/TicTacToe/Helpers/TicTacToeHelper.js
+++ src/components/TicTacToe/Helpers/TicTacToeHelper.js
@@ -1,6 +1,33 @@
 
 class TTThelper {
 
+    initialState = {
+        game: {
+            gameState: undefined,
+            moves: 0,
+            moveseq: [],
+            tiles: [0,0,0,0,0,0,0,0,0]
+        },
+        messaging:{
+            message: "Press \"Start Game\" to begin!",
+            messageClass: "default"
+        }
+    };
+
+    // should be private
+    currentState = this.initialState;
+
+    getCurrentState = () => {
+        return this.currentState;
+    };
+
+    setCurrentState = (stateobj) => {
+        this.currentState = stateobj;
+    };
+
+    whoMove = (moves) => {
+        return moves % 2 === 0 ? "X" : "O";
+    }
 
 }
 
--- src/components/TicTacToe/Helpers/TicTacToeHelper.test.js
+++ src/components/TicTacToe/Helpers/TicTacToeHelper.test.js
@@ -1,34 +1,20 @@
 import TTTHelper from "./TicTacToeHelper"
 
-let mockInitialState = {
-    game: {
-        gameState: undefined,
-        moves: 0,
-        moveseq: [],
-        tiles: [0,0,0,0,0,0,0,0,0]
-    },
-    messaging:{
-        message: "Press \"Start Game\" to begin!",
-        messageClass: "default"
-    }
-};
-
 let ttthelp = new TTTHelper();
 
-
 it('should return an X based on even move count', () => {
-    const move = TTTHelper.whoMove(2);
+    const move = ttthelp.whoMove(2);
     expect(move).toBe('X');
 });
 
-it('should return an O based on even move count', () => {
-    const move = TTTHelper.whoMove(3);
+it('should return an O based on odd move count', () => {
+    const move = ttthelp.whoMove(3);
     expect(move).toBe('O');
 });
 
 it('should set initial state on load and validate setCurrentState, getCurrentState', () => {
-    let initialState = ttthelp.setCurrentState(mockInitialState);
-    expect(initialState).toEqual({
+
+    let mockInitialState = {
         game: {
             gameState: undefined,
             moves: 0,
@@ -37,13 +23,25 @@ it('should set initial state on load and validate setCurrentState, getCurrentSta
         },
         messaging:{
             message: "Press \"Start Game\" to begin!",
-            messageClass: "default"
+            messageClass: "default-test"
+        }
+    };
+
+    ttthelp.setCurrentState(mockInitialState);
+    expect(ttthelp.currentState).toEqual({
+        game: {
+            gameState: undefined,
+            moves: 0,
+            moveseq: [],
+            tiles: [0,0,0,0,0,0,0,0,0]
+        },
+        messaging:{
+            message: "Press \"Start Game\" to begin!",
+            messageClass: "default-test"
         }
     });
 
-    let currentState = ttthelp.getCurrentState();
-
-    expect(currentState).toEqual({
+    expect(ttthelp.getCurrentState()).toEqual({
         game: {
             gameState: undefined,
             moves: 0,
@@ -52,7 +50,7 @@ it('should set initial state on load and validate setCurrentState, getCurrentSta
         },
         messaging:{
             message: "Press \"Start Game\" to begin!",
-            messageClass: "default"
+            messageClass: "default-test"
         }
     });
 });

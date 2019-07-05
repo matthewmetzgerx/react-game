2d4b4c45f20ea5520461a3d6530000ddafcb3d64
Matthew Metzger <mmetzger@my.bellevue.edu>
2019-07-04 20:35:51 -0400

    Added buttons for reset-game and revert-move and corrected their respective tests. Events must be wired up in the helper.

--- src/components/TicTacToe/Controller/Controller.js
+++ src/components/TicTacToe/Controller/Controller.js
@@ -12,7 +12,14 @@ export default class Controller extends Component {
         if (this.props.gameState === undefined){
             return(
                 <div className={"controller"}>
-                    <Button gameState={this.props.gameState} text="Start Game" event="start-game" cb={this.callback} />
+                    <Button gameState={this.props.gameState} className="start-game" text="Start Game" event="start-game" cb={this.callback} />
+                </div>
+            )
+        } else if ((this.props.gameState === "in-progress") && (this.props.moves > 0)){
+            return(
+                <div className={"controller"}>
+                    <Button gameState={this.props.gameState} className="reset-game" text="Reset Game" event="reset-game" cb={this.callback} />
+                    <Button gameState={this.props.gameState} className="revert-move" text="Revert Move" event="revert-move" cb={this.callback} />
                 </div>
             )
         } else {
--- src/components/TicTacToe/Controller/Controller.test.js
+++ src/components/TicTacToe/Controller/Controller.test.js
@@ -3,8 +3,27 @@ import Controller from "./Controller";
 import {mount} from "enzyme/build";
 import Button from "../../Elements/Button/Button";
 
-it('renders start-game button in controller reading undefined gameState and empty moves array', () => {
-    let myprops = {gameState: undefined, moves:[]};
+it('renders start-game button in controller reading undefined gameState and empty moves', () => {
+    let myprops = {gameState: undefined, moves:0};
     const controller = mount( <Controller {...myprops} />);
-    expect(controller.find(Button)).toHaveLength(1);
+    expect(controller.find(Button).hasClass("start-game")).toEqual(true);
+});
+
+it('renders reset-game button in controller reading "in-progress" gameState and moves greater than 0', () => {
+    let myprops = {gameState: "in-progress", moves:1};
+    const controller = mount( <Controller {...myprops} />);
+    expect(controller.find(Button).first().hasClass("reset-game")).toEqual(true);
+});
+
+it('renders revert-move button in controller reading "in-progress" gameState and moves greater than 0', () => {
+    let myprops = {gameState: "in-progress", moves:1};
+    const controller = mount( <Controller {...myprops} />);
+    expect(controller.find(Button).last().hasClass("revert-move")).toEqual(true);
+});
+
+
+it('renders no buttons in controller reading "in-progress" gameState and moves == 0', () => {
+    let myprops = {gameState: "in-progress", moves:0};
+    const controller = mount( <Controller {...myprops} />);
+    expect(controller.find(Button)).toEqual({});
 });
